f91b14353f9a41772b3ccd075d6e2fdb551045d7
Areskul <fullbringer2.0@gmail.com>
2020-08-21 13:55:41 +0200

    color palette mixins

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div class="big"></div>
-    <Card color="error-darken2"></Card>
+    <Card color="error-darken5"></Card>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -14,10 +14,9 @@
   </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
+<script>
 import Card from "@/components/containers/Card.vue";
-export default Vue.extend({
+export default {
   name: "HelloWorld",
   props: {
     msg: String,
@@ -25,7 +24,7 @@ export default Vue.extend({
   components: {
     Card,
   },
-});
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import Vue from "vue";
+import store from "./store";
+import router from "./router";
 import App from "./App.vue";
+import "@/styles/colors/light.scss";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
+  router,
   render: (h) => h(App),
 }).$mount("#app");
