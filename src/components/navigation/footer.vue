32740a78539be0e8f5b8f55971e69254b85f0d71
Areskul <the.only.areskul@gmail.com>
2020-09-20 22:23:11 +0200

    header back to normal

--- src/components/navigation/footer.vue
+++ src/components/navigation/footer.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="columns">
     <div class="column">
-      <card color="primary" height="100px">
-        <icon />
-      </card>
+      <card color="ternary" height="100px"></card>
     </div>
   </div>
 </template>
 <script>
 import card from "../containers/card";
-import icon from "../buttons/icon";
 export default {
   name: "vheader",
   components: {
     card,
-    icon,
   },
 };
 </script>
