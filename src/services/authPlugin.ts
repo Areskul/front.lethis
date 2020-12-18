056d581c5b2f963272417f0c2f8b4ffa015b9e1d
Areskul <the.only.areskul@gmail.com>
2020-12-18 22:50:44 +0100

    better token

--- src/services/authPlugin.ts
+++ src/services/authPlugin.ts
@@ -2,7 +2,11 @@ import { definePlugin } from "villus";
 export const authPlugin = (config: { token: string }): any => {
   return definePlugin(({ opContext }) => {
     if (config.token != null) {
-      opContext.headers = { Authorization: "Bearer <" + config.token + ">" };
+      opContext.headers = {
+        Authorization: "Bearer " + config.token,
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      };
     }
   });
 };
