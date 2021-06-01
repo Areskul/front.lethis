cd625c53e068802ad038f1c0d544899e6eda9dd2
Areskul <the.only.areskul@gmail.com>
2021-06-01 23:46:03 +0200

    charges computed

--- src/components/input/charges.vue
+++ src/components/input/charges.vue
@@ -1,7 +1,5 @@
 <template lang="pug">
 .container
-  .flex.justify-center.items-center.py-6
-    h1 Detail des charges
   form#form
     .flex.flex-wrap.justify-center
       .flex(v-for="{ name, as, label, ...attrs } in schema.fields")
@@ -16,18 +14,11 @@
             :readonly="attrs.computed",
             v-model="models[attrs.modelkey]"
           )
-          Field(
-            v-else,
-            :as="as",
-            :id="name",
-            :name="name",
-            v-bind="attrs",
-            :readonly="attrs.computed"
-          )
+          Field(v-else, :as="as", :id="name", :name="name", v-bind="attrs")
         ErrorMessage(:name="name")
 </template>
 <script lang="ts">
-import { defineComponent, computed, watch, ref } from "vue";
+import { defineComponent, computed, ref } from "vue";
 import { Field, ErrorMessage } from "vee-validate";
 import * as yup from "yup";
 import { clientUtils } from "@/composables/client";
@@ -46,63 +37,66 @@ export default defineComponent({
   },
   setup() {
     const { saveOnLeave, client } = clientUtils();
-    const incomes = computed(() => {
-      if (client.value.incomes) {
-        return client.value.incomes;
+    const charges = computed(() => {
+      if (client.value.charges) {
+        return client.value.charges;
       } else {
         return {
-          benefits: null,
-          wage: null,
-          landed: null,
-          others: null,
-          joint: null,
-          total: null,
+          rent: "0",
+          food: "0",
+          energy: "0",
+          isp: "0",
+          school: "0",
+          insurance: "0",
+          help: "0",
+          transports: "0",
+          recreation: "0",
+          holidays: "0",
+          consumer: "0",
+          others: "0",
+          coownership: "0",
+          total: "0",
+          qp: "0",
+          result: "0",
         };
       }
-    });
-    let total = null;
-    const models = ref({
-      total: total,
-    });
-    watch(incomes, () => {
-      incomes.value.total = incomes.value.benefits + incomes.value.wages;
     });
     //Vee-validate
     const schema: FormSchema = {
       fields: [
         {
           as: "input",
-          name: "incomes.benefits",
+          name: "charges.rent",
           label: "Loyer",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.wage",
+          name: "charges.food",
           label: "Alimentaire",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.landed",
+          name: "charges.energy",
           label: "Eau/Électrivité/Gaz",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.others",
+          name: "charges.isp",
           label: "Téléphone/Internet",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.joint",
+          name: "charges.school",
           label: "Frais de scolarité",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.total",
+          name: "charges.insurance",
           label: "Assurances IARD",
           type: "text",
           computed: true,
@@ -110,49 +104,49 @@ export default defineComponent({
         },
         {
           as: "input",
-          name: "incomes.qp",
+          name: "charges.help",
           label: "Aide à Domicile",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.result",
+          name: "charges.transports",
           label: "Transports",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.result",
+          name: "charges.recreation",
           label: "Loisirs",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.result",
+          name: "charges.holidays",
           label: "Vacances Annuelles",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.result",
+          name: "charges.consumer",
           label: "Crédit consommation",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.result",
+          name: "charges.others",
           label: "Autres",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.result",
+          name: "charges.coownership",
           label: "Charges de copropriétés",
           type: "text",
         },
         {
           as: "input",
-          name: "incomes.total",
+          name: "charges.total",
           label: "Total",
           type: "text",
           computed: true,
@@ -160,7 +154,7 @@ export default defineComponent({
         },
         {
           as: "input",
-          name: "incomes.total",
+          name: "charges.qp",
           label: "Quote Part",
           type: "text",
           computed: true,
@@ -168,7 +162,7 @@ export default defineComponent({
         },
         {
           as: "input",
-          name: "incomes.total",
+          name: "charges.result",
           label: "Résultat",
           type: "text",
           computed: true,
@@ -176,22 +170,40 @@ export default defineComponent({
         },
       ],
       validation: yup.object({
-        incomes: yup.object({
-          benefits: yup.number(),
-          wage: yup.number(),
-          landed: yup.number(),
+        charges: yup.object({
+          rent: yup.number(),
+          food: yup.number(),
+          energy: yup.number(),
+          isp: yup.number(),
+          school: yup.number(),
+          insurance: yup.number(),
+          help: yup.number(),
+          transports: yup.number(),
+          recreation: yup.number(),
+          holidays: yup.number(),
+          consumer: yup.number(),
           others: yup.number(),
-          joint: yup.number(),
+          coownership: yup.number(),
           total: yup.number(),
           qp: yup.number(),
           result: yup.number(),
         }),
       }),
     };
+    const total = computed(() => {
+      const res = new String(
+        parseFloat(charges.value.benefits) + parseFloat(charges.value.wage)
+      );
+      charges.value.total = res;
+      return res;
+    });
+    const models = ref({
+      total: total,
+    });
     saveOnLeave(schema);
     return {
       client,
-      incomes,
+      charges,
       schema,
       models,
     };
